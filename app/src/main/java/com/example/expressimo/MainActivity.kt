4834cebab7581832d0b33889104b7528486aaca1
vibhor <vibhor27061995@gmail.com>
2023-10-02 18:43:37 +0530

    Authentication Commit

--- app/src/main/java/com/example/expressimo/MainActivity.kt
+++ app/src/main/java/com/example/expressimo/MainActivity.kt
@@ -1,13 +1,50 @@
 package com.example.expressimo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object{
+        private const val TAG = "MainActivity"
+    }
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.miLogout){
+            Log.i(TAG, "Logout")
+
+            // Logout the user
+            auth.signOut()
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+
+            //Clear the whole backstack
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 }
