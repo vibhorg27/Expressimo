dcdc6dccfb04e5dd2271e8cc8d314693060f4d40
vibhor <vibhor27061995@gmail.com>
2023-10-03 14:32:45 +0530

    recycler view commit

--- app/src/main/java/com/example/expressimo/MainActivity.kt
+++ app/src/main/java/com/example/expressimo/MainActivity.kt
@@ -1,26 +1,66 @@
 package com.example.expressimo
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+data class User(
+    val displayName: String= "",
+    val emojis: String= ""
+)
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 class MainActivity : AppCompatActivity() {
 
     private companion object{
         private const val TAG = "MainActivity"
     }
     private lateinit var auth: FirebaseAuth
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = Firebase.auth
+        val query = db.collection("users")
+        val options = FirestoreRecyclerOptions.Builder<User>().setQuery(query, User::class.java)
+            .setLifecycleOwner(this).build()
+
+        val adapter= object: FirestoreRecyclerAdapter<User, UserViewHolder> (options){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+                val view = LayoutInflater.from(this@MainActivity).inflate(android.R.layout.simple_list_item_2, parent, false)
+                return UserViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: User) {
+                val tvName: TextView = holder.itemView.findViewById(android.R.id.text1)
+                val tvEmojis: TextView = holder.itemView.findViewById(android.R.id.text2)
+                tvName.text = model.displayName
+                tvEmojis.text = model.emojis
+            }
+
+        }
+        findViewById<RecyclerView>(R.id.rvUsers).adapter = adapter
+        findViewById<RecyclerView>(R.id.rvUsers).layoutManager = LinearLayoutManager(this)
 
 
     }
@@ -43,8 +83,26 @@ class MainActivity : AppCompatActivity() {
             logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(logoutIntent)
         }
+        else if(item.itemId == R.id.miEdit){
+            Log.i(TAG, "Show alert dialog to edit status")
+            showAlertDialog()
+        }
 
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAlertDialog() {
+        val editText = EditText(this)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Update your Emojis")
+            .setView(editText)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("Ok", null)
+            .show()
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener{
+            Log.i(TAG, "clicked on positive button")
+        }
     }
 }
